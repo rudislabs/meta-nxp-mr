3e84fd37fe870271e290f8e8dc02b88aa47f2599
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2026-01-08 00:39:16 -0500

    docs: update README and build script for BSP 6.12.49-2.2.0
    Update documentation and build script for the new BSP version.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,12 @@ fi
 
 BASE_WORK_DIR="$HOME/nxp-mr-image"
 BUILD_TYPE=jazzy
-BRANCH=lf-6.6.23-2.0.0-scarthgap
-MACHINE="imx8mpnavq"
-MANIFEST="imx-6.6.23-2.0.0.xml"
-DISTRO="imx-desktop-xwayland"
+BRANCH=lf-6.12.49-2.2.0-walnascar-mr
+MACHINE="imx8mpnavqdesktop"
+MANIFEST="imx-6.12.49-2.2.0-navqplus.xml"
+DISTRO="fsl-imx-xwayland"
 SETUP="imx-setup-release.sh"
-BUILDDIR="build-desktop"
-NXP_META_IMAGE="meta-nxp-mr"
+BUILDDIR="build"
 BUILD=`date +%Y%m%d.%H%M`; start=`date +%s`
 
 mkdir -p ~/bin
@@ -24,56 +23,25 @@ PATH=${PATH}:~/bin
 mkdir -p ${BASE_WORK_DIR}
 cd ${BASE_WORK_DIR}
 repo init \
-    -u https://github.com/nxp-imx/imx-manifest.git \
-    -b imx-linux-scarthgap \
+    -u https://github.com/rudislabs/mr-imx-manifest.git \
+    -b ${BRANCH} \
     -m ${MANIFEST}
 
 repo sync -j`nproc`
 
-DISTRO=${DISTRO} MACHINE=${MACHINE} EULA=yes source ${SETUP} -b ${BUILDDIR} || exit $?
+DISTRO=${DISTRO} MACHINE=${MACHINE} EULA=1 source ${SETUP} -b ${BUILDDIR} || exit $?
 
 get_yocto_hash() {
     local githash=$(git rev-parse --short=10 HEAD)
     echo "$githash"
 }
 
-cd ${BASE_WORK_DIR}/sources
-for i in ${NXP_META_IMAGE}; do
-    if [ -d ${i} ]; then
-        pushd ${i}
-        git pull
-        popd
-    else
-        git clone -b $BRANCH git@github.com:NXPHoverGames/${i}.git || exit $?
-    fi
-    if [ $i = "${NXP_META_IMAGE}" ]; then
-        pushd $i
-            yocto_hash=$(get_yocto_hash)
-        popd
-    fi
-done
+cd ${BASE_WORK_DIR}/sources/meta-nxp-mr
+yocto_hash=$(get_yocto_hash)
 
 RELEASE_VER="${BUILD_TYPE}-$(date +%y%m%d%H%M%S)-${yocto_hash}"
 
-cd ${BASE_WORK_DIR}/build-desktop/conf
-if ! grep -q "/sources/${NXP_META_IMAGE}" bblayers.conf
-then
-cat >> bblayers.conf <<EOF
-BBLAYERS += "\${BSPDIR}/sources/${NXP_META_IMAGE}"
-EOF
-fi
-if ! grep -q "MACHINE ??= 'imx8mpnavqdesktop'" local.conf
-then
-sed -i "s/^MACHINE ??=.*/MACHINE ??= 'imx8mpnavqdesktop'/" local.conf
-fi
-if ! grep -q "DISTRO ?= 'imx-desktop-xwayland'" local.conf
-then
-sed -i "s/^DISTRO ?=.*/DISTRO ?= 'imx-desktop-xwayland'/" local.conf
-fi
-if ! grep -q 'PACKAGE_CLASSES = "package_rpm"' local.conf
-then
-sed -i 's/^PACKAGE_CLASSES =.*/PACKAGE_CLASSES = "package_rpm"/' local.conf
-fi
+cd ${BASE_WORK_DIR}/${BUILDDIR}/conf
 if ! grep -q 'PARALLEL_MAKE =' local.conf
 then
 cat >> local.conf <<EOF
@@ -93,20 +61,10 @@ KERNEL_LOCALVERSION = "$RELEASE_VER"
 EOF
 fi
 
-echo $RELEASE_VER > ${BASE_WORK_DIR}/sources/${NXP_META_IMAGE}/recipes-fsl/images/files/release || exit $?
-
-for i in ${BUILDDIR}/../sources/meta-imx/meta-imx-bsp/recipes-kernel/linux/linux-imx_6.6.bb \
-     ${BUILDDIR}/../sources/meta-imx/meta-imx-bsp/recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv_6.4.11.p2.6.bb;
-do
-    sed -i "s/^LOCALVERSION\s*=.*/LOCALVERSION = \"-${RELEASE_VER}\"/" ${i}
-    if [ "x$(grep '^LOCALVERSION\s*=' ${i})" = "x" ]; then
-        echo "LOCALVERSION = \"-$RELEASE_VER\"" >> ${i} || exit $?
-    fi
-done
+echo $RELEASE_VER > ${BASE_WORK_DIR}/sources/meta-nxp-mr/recipes-fsl/images/files/release || exit $?
 
 cd ${BASE_WORK_DIR}
-source setup-environment build-desktop
-bitbake imx-image-ros -c cleansstate
+source sources/poky/oe-init-build-env ${BUILDDIR}
 bitbake imx-image-ros
 
 finish=`date +%s`; echo "### Build Time = `expr \( $finish - $start \) / 60` minutes"
